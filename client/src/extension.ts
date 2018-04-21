975add070e2d4943a0a5c40b9af4e1826d42ed61
Umoxfo <ycco34vx@gmail.com>
2018-04-21 10:26:55 -0700

    Move JRE.ts to the client

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -8,34 +8,38 @@ import { ExtensionContext } from "vscode";
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from "vscode-languageclient";
 
 import * as path from "path";
+import { checkJRE } from "./JRE";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    // The server is implemented in node
-    const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
+    // JRE check
+    checkJRE().then(() => {
+        // The server is implemented in node
+        const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
 
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: { execArgv: ["--nolazy", "--inspect=6009"] },
-        },
-    };
+        // If the extension is launched in debug mode then the debug server options are used
+        // Otherwise the run options are used
+        const serverOptions: ServerOptions = {
+            run: { module: serverModule, transport: TransportKind.ipc },
+            debug: {
+                module: serverModule,
+                transport: TransportKind.ipc,
+                options: { execArgv: ["--nolazy", "--inspect=6009"] },
+            },
+        };
 
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        // Register the server for HTML documents
-        documentSelector: [{ language: "html" }],
-    };
+        // Options to control the language client
+        const clientOptions: LanguageClientOptions = {
+            // Register the server for HTML documents
+            documentSelector: [{ language: "html" }],
+        };
 
-    // Create the language client and start the client.
-    client = new LanguageClient("w3cvalidation", "HTML Validation Service", serverOptions, clientOptions);
-    client.registerProposedFeatures();
-    client.start();
+        // Create the language client and start the client.
+        client = new LanguageClient("w3cvalidation", "HTML Validation Service", serverOptions, clientOptions);
+        client.registerProposedFeatures();
+        client.start();
+    });
 }
 
 export function deactivate(): Thenable<void> {
