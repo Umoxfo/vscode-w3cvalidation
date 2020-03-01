c5b26a6e374a2592802443d5400e647572656446
Umoxfo <ycco34vx@gmail.com>
2020-03-01 13:20:42 -0330

    webpack: Replace with own json-perf-loader

---
+++ build/json-perf-loader/src/json-perf-loader.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Makoto Sakaguchi. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
+
+"use strict";
+
+import { getOptions } from "loader-utils";
+// import * as schema from "./options.json";
+
+// eslint-disable-next-line prettier/prettier
+import type { loader } from "webpack";
+
+// Default limit: 10 KiB
+const DEFAULT_OPTIONS = { limit: 10_240 };
+
+export default function jsonPerfLoader(this: loader.LoaderContext, source: unknown): void {
+    const callback = this.async() as loader.loaderCallback;
+
+    // For Webpack 5
+    // this.getOptions(schema)
+    const options = { ...DEFAULT_OPTIONS, ...getOptions(this) };
+
+    let value = "";
+    try {
+        const tmp = typeof source === "string" ? JSON.parse(source) : source;
+        value = JSON.stringify(tmp);
+    } catch (error) {
+        this.emitError(error);
+    }
+
+    // https://v8.dev/blog/cost-of-javascript-2019#json
+    if (value.length < options.limit) {
+        value = value.replace(/(\u2028)|(\u2029)/g,
+            (substr: string, p1: string, p2: string) => p1 ? "\\u2028" : (p2 ? "\\u2029" : substr));
+
+        callback(null, `module.exports = ${value}`);
+        return;
+    }
+
+    callback(null, `module.exports = JSON.parse(${value})`);
+}
