68b2d53133019adc0d10804a49651bd461c22f46
Umoxfo <ycco34vx@gmail.com>
2022-04-04 13:40:51 -0400

    Update runTest.ts
    Fix the extension development path.
    Fix fallback when switching to Win64.

--- test/src/runTest.ts
+++ test/src/runTest.ts
@@ -1,15 +1,13 @@
-import path from "path";
-
 import { runTests } from "@vscode/test-electron";
 
+import path from "path";
+
 const VSCODE_VERSION = "1.65.2";
-// Use win64 instead of win32 for testing Windows
-const platform = process.platform === "win32" ? "win32-x64-archive" : "win32-archive";
 
 async function main(): Promise<void> {
     try {
         // The folder containing the Extension Manifest package.json
-        const extensionDevelopmentPath = path.resolve(__dirname, "../../../");
+        const extensionDevelopmentPath = path.resolve(__dirname, "../../");
 
         // The path to the extension test runner script
         const extensionTestsPath = path.resolve(__dirname, "./suite/index");
@@ -17,7 +15,8 @@ async function main(): Promise<void> {
         // Run the extension test
         await runTests({
             version: VSCODE_VERSION,
-            platform,
+            // Use win64 instead of win32 for testing Windows
+            platform: getPlatform(),
             extensionDevelopmentPath,
             extensionTestsPath,
             launchArgs: ["--disable-extensions", path.resolve(__dirname, "../resource")],
@@ -25,9 +24,20 @@ async function main(): Promise<void> {
     } catch (err) {
         // eslint-disable-next-line no-console
         console.error(err);
-        // eslint-disable-next-line no-console
-        console.error("Failed to run tests");
         process.exit(1);
+    }
+
+    function getPlatform() {
+        switch (process.platform) {
+            case "win32":
+                return "win32-x64-archive";
+            case "darwin":
+                return "darwin";
+            case "linux":
+                return "linux-x64";
+            default:
+                return "";
+        }
     }
 }
 
